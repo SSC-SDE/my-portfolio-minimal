7f2a3645ff46fa9706188ff435d676d26a582af3
Shankhya Subhra Chatterjee <shankhyasubhrac@outlook.com>
2025-03-09 14:36:09 +0530

    tring to deploy again

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,7 +24,7 @@ export const Hero = () => {
       repeat: Infinity,
       repeatType: "mirror",
     });
-  }, []);
+  });
 
   const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #000 50%, ${color})`;
   const border = useMotionTemplate`1px solid ${color}`;
